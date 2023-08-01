eb313595b66db617640c867e2e78d1ad9159f400
Olivier <kleinolivier@orange.fr>
2023-08-01 15:43:14 +0200

    :construction: fin de la construction du dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import { useAuth } from '../Contexts/AuthContext';
 
 const Dashboard = () => {
-    
+    const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth();
+
+    const logIn = (e) =>{
+        e.preventDefault()
+        setIsLoggedIn(true)
+        setAuthUser({
+            Name: 'John Doe'
+        })
+    }
+
+    const logOut = (e) =>{
+        e.preventDefault()
+        setIsLoggedIn(false)
+        setAuthUser(null)
+    }
+
     return (
-        <div>dashboard</div>
+        <>
+            <span>User is currently: {isLoggedIn ? 'Logged-In' : 'Logged-Out'}. </span>
+            {isLoggedIn ? (<span>User name: {authUser.Name}</span>) : null}
+            <br/>
+            {isLoggedIn
+                ? <button onClick={(e)=>{logOut(e)}}>Log Out</button>
+                : <button onClick={(e)=>{logIn(e)}}>Log In</button>
+            }
+        </>
     )
 }
 
